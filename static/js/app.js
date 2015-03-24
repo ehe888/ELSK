81c5f0cae887fa666a75f91912d72721eb7129a8
wan1wan2 <wangjiajun1117@gmail.com>
2015-03-24 15:27:39 +0800

    20150324
    store map

--- static/js/app.js
+++ static/js/app.js
@@ -42,8 +42,12 @@ $(function(){
 	$("#power-products1").html(product);
 	$("#power-products2").html(product);
 
-	var product_series = new EJS({url: 'js/product-series.ejs'}).render({"number":8});
+	var product_series = new EJS({url: 'js/product-series.ejs'}).render({"number":11});
 	$("#product-productseries").html(product_series);
+	$("#search-productseries").html(product_series);
+
+	var storeList = new EJS({url: 'js/stores.ejs'}).render();
+	$("#store_list").html(storeList);
 
 
 	$(".baby-video-series").html(new EJS({url: 'js/baby-video-preview.ejs'}).render());
